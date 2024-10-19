7ad90c12a6b79a5b3003caa86d8535ee8773f926
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-10-20 02:04:43 +0900

    Add new files

---
+++ src/client/java/karasu_lab/fabric_example/FabricExampleModClient.java
@@ -0,0 +1,10 @@
+package karasu_lab.fabric_example;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabricExampleModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
